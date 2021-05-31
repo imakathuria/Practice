5c2d37724728f2e6bf8ff594af463f2407ee258a
Akhil <imakathuria@gmail.com>
2021-05-31 15:46:02 +0530

    changes done

--- making header files for project work/linkedlist/linkedlistuse.cpp
+++ making header files for project work/linkedlist/linkedlistuse.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
 #include "linkedlist.h"
+void print(node*head){
+    while(head){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
 int main(){
     // statically
     node n1(10);
@@ -9,9 +15,11 @@ int main(){
     node n2(20);
     n1.next = &n2;
     // cout<<n1.data<<" "<<n2.data<<endl;
-    while(head){
-        cout<<head->data<<" ";
-        head=head->next;
-    }
+    
+    // dynamically
+    node*obj=new node(30);
+    obj->next=&n1;
+    head=obj;
+    print(head);
     return 0;
 }
