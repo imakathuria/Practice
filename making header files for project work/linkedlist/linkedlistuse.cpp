38c342d4dd5c9b36f211b210e61adf92c3e947fe
Akhil <imakathuria@gmail.com>
2021-06-01 02:33:50 +0530

    late night commit

--- making header files for project work/linkedlist/linkedlistuse.cpp
+++ making header files for project work/linkedlist/linkedlistuse.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 #include "linkedlist.h"
+/*
+1 2 3 4 5
+*/
 node*takeInput(){
     int data;
     cin>>data;
@@ -24,8 +27,27 @@ void print(node*head){
         head=head->next;
     }
 }
+node* insert(int index,node*head,int element){
+    node*newnode=new node(element);
+    if(index==0){
+        newnode->next=head;
+        head=newnode;
+        
+    }else{
+        node*temp=head;
+        while(index-1>0){
+            temp=temp->next;
+            index--;
+        }
+        newnode->next=temp->next;
+        temp->next=newnode;
+    }
+    return head;
+}
 int main(){
     node*head=takeInput();
+    print(head);
+    insert(0,head,100);
     print(head);
     return 0;
 }
