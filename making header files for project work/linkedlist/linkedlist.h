89dba6cb4982f6a0f12484b49fbcede5dfaace97
Akhil <imakathuria@gmail.com>
2021-06-01 11:40:09 +0530

    all fuctions part 1 added to linked list 1. insert 2. delete 3. insert_node_recursively 4. deleteNode_recursively

--- making header files for project work/linkedlist/linkedlist.h
+++ making header files for project work/linkedlist/linkedlist.h
@@ -4,7 +4,6 @@ class node{
     public:
     int data;
     node*next;
-    node*head;
     node(int data){
         this->data=data;
         this->next=NULL;
--- making header files for project work/linkedlist/linkedlistuse.cpp
+++ making header files for project work/linkedlist/linkedlistuse.cpp
@@ -32,22 +32,94 @@ node* insert(int index,node*head,int element){
     if(index==0){
         newnode->next=head;
         head=newnode;
-        
+        return head;
     }else{
         node*temp=head;
-        while(index-1>0){
+        while(temp and index-1>0){
             temp=temp->next;
             index--;
         }
-        newnode->next=temp->next;
-        temp->next=newnode;
+        if(temp){
+            newnode->next=temp->next;
+            temp->next=newnode;
+            return head;
+        }else{
+            return head;
+        }
     }
+    return head;
+}
+node*deletenode(node*head,int index){
+    if(index==0){
+        if(!head)return head;
+        node*a=head;
+        head=head->next;
+        delete a;
+        return head;
+    }
+    else{
+        int count=0;
+        node*temp=head;
+        while(temp && count<index-1){
+            temp=temp->next;
+            count++;
+        }
+        if(!temp){
+            return head;
+        }
+        node*b=temp->next;
+        if(temp->next){
+            temp->next=temp->next->next;
+        }
+        delete b;
+        return head;
+    }
+}
+node*insert_node_recursively(node*head,int index,int val){
+    if(!head)return head;
+    if(index==0){
+        node*newnode=new node(val);
+        newnode->next=head;
+        head=newnode;
+        return head;
+    }
+    node*sa=insert_node_recursively(head->next,index-1,val);
+    head->next=sa;
+    return head;
+
+}
+
+node*deleteNode_recursively(node*head,int index){
+    if(head==NULL){
+        return head;
+    }
+    if(index==0){
+        node*a=head;
+        head=head->next;
+        delete a;
+    }
+    node*sa=deleteNode_recursively(head->next,index-1);
+    head->next=sa;
     return head;
 }
 int main(){
     node*head=takeInput();
     print(head);
-    insert(0,head,100);
+    cout<<endl;
+    head=insert(0,head,100);
     print(head);
+    cout<<endl;
+    head=deletenode(head,0);
+    print(head);
+    cout<<endl;
+    head=deleteNode_recursively(head,0);
+    print(head);
+    cout<<endl;
+    head=insert_node_recursively(head,3,500);
+    print(head);
+    cout<<endl;
+    head=insert_node_recursively(head,0,1000);
+    print(head);
+    cout<<endl;
     return 0;
 }
